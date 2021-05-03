6b77fd2ae2c8816c00887b8683b4d73838e66dc6
Sankalp-02 <bsmurthy2002@gmail.com>
2021-05-03 17:31:28 +0530

    sandcode - credit:vamshi

---
+++ autism.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define daily(i, n) for (int i = 0; i < n; i++)
+#define real true
+#define fake false
+#define best long long
+#define over endl
+typedef vector<ll> gtodiayuh;
+#define based \
+    int t;    \
+    cin >> t; \
+    while (t--)
+#define gay make_pair
+#define actual_autism cin.tie(0)->sync_with_stdio(0)
+#define om return
+#define nama 0
+
+int main(void)
+{
+    actual_autism;
+    // Your code goes here retard
+    om nama;
+}
